9fe7b9f154e14a6cfe572ae8fb62da849ecb3b8e
Jaedan Blechinger-Slocum <jblech2020@gmail.com>
2020-03-01 18:47:32 -0500

    6/50

---
+++ src/Definitions.java
@@ -0,0 +1,19 @@
+
+public class Definitions {
+	
+	private String definition;
+	private String partOfSpeech;
+
+	public void definitions(String definition, String partsOfSpeech) {
+		this.definition = definition;
+		this.partOfSpeech = partsOfSpeech;
+	}
+
+	public String getDefinition() {
+		return this.definition;
+	}
+	
+	public String getPartOfSpeech() {
+		return this.partOfSpeech;
+	}
+}
