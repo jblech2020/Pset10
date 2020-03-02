677e8007256f57a59feaa83f1d82b1b2111d0b9f
Jaedan Blechinger-Slocum <jblech2020@gmail.com>
2020-03-01 19:55:49 -0500

    7/50

---
+++ src/Dictionary.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+public class Dictionary {
+	
+	public static Words[] wordList;
+	public static Words[] addAllWords() throws JsonSyntaxException, JsonIOException, FileNotFoundException {
+		wordList = new Gson().fromJson(new FileReader(".\\JSON\\words.json"), Words[].class);
+		return wordList;
+	}
+
+	public static ArrayList<String> listWords() {
+		ArrayList<String> listOfWords = new ArrayList<String>();
+		for(Words word : wordList) {
+			listOfWords.add(word.getSpelling());
+		}
+		return listOfWords;
+	}
+}
